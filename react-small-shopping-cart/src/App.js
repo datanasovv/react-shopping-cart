281a4e3158b6fd3fe2f3791843b6f06d642cea79
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2022-02-15 15:42:44 +0200

    created Header,Basic,Main components and render sample products

---
+++ react-small-shopping-cart/src/App.js
@@ -0,0 +1,19 @@
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import data from "./data";
+
+function App() {
+  const { products } = data;
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className="row">
+        <Main products={products}></Main>
+        <Basket></Basket>
+      </div>
+    </div>
+  );
+}
+
+export default App;
